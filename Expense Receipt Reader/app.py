ce1a15190c377caf15bf9b683427a4312568f3c4
VenkatTetali02 <99071408+VenkatTetali02@users.noreply.github.com>
2025-05-07 08:57:35 -0700

    Updating the file path

--- Expense Receipt Reader/app.py
+++ Expense Receipt Reader/app.py
@@ -2,7 +2,7 @@ from Tasks import ExpenseReceiptTasks
 from Agents import ExpenseReceiptAgents
 from crewai import Crew,Process
 
-file_path="./content/" + "Hotel_Receipt1.pdf"
+file_path=""Hotel_Receipt.pdf"
 
 #get the agents
 ExpenseAgents=ExpenseReceiptAgents()
